64a2db57e2d02db9ea311fa52596236ff6c1a342
CookieMonz1er <cengizkutay134@gmail.com>
2026-02-27 17:53:11 +0300

    Methods deposit, withdraw, transferTo, displayAccountInfo added. Fix addTransaction!

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -6,7 +6,7 @@ public class BankAccount {
     private int count;
     private String[] transactionHistory;
 
-    public BankAccount(String ownerName, long iban, String[] transactionHistory) {
+    public BankAccount(String ownerName, long iban) {
         this.ownerName = ownerName;
         this.iban = iban;
         balance = 0;
@@ -61,5 +61,31 @@ public class BankAccount {
             transactionHistory[count - 1] = record;
         }
 
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+        addTransaction("DEPOSIT: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance){
+            addTransaction("FAILED: " + amount);
+        }
+        balance -= amount;
+        addTransaction("WITHDRAW: " + amount);
+    }
+
+    public void transferTo(long iban, BankAccount receiver, int amount){
+        if(receiver.getIban() == iban && amount <= this.getBalance()){
+            balance -= amount;
+            receiver.setBalance(receiver.getBalance()+amount);
+            receiver.addTransaction("GOT: " + amount);
+            addTransaction("SENT: " + amount);
+        }
+    }
+
+    public void displayAccountInfo(){
+        System.out.println("(" + ownerName + ", " + iban + ", " + balance + ")");
     }
 }
